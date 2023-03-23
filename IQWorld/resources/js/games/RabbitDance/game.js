a63038958ba5b9b7162537481479b682b88f864c
Samuel Fernandez <113102356+Samfernande@users.noreply.github.com>
2023-03-23 12:13:33 +0100

    Ajout Interface RabbitDance

--- IQWorld/resources/js/games/RabbitDance/game.js
+++ IQWorld/resources/js/games/RabbitDance/game.js
@@ -1,4 +1,5 @@
 import { Rabbit } from './rabbit.js';
+import { Interface } from './interface.js';
 
 export class Game 
 {
@@ -6,54 +7,65 @@ export class Game
     constructor() {
         this.rabbitsLeft = [];
         this.rabbitsRight = [];
+
+        // Récupération de l'interface
+        this.interfaceGame = new Interface();
+
+        // Récupération du canvas
+        this.canvas = document.getElementById("canvasGames");
+        this.ctx = this.canvas.getContext("2d");
     }
 
     rand(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
       }
 
-    createRabbits(canvas)
+    createRabbits()
     {
-        for (var i = 0; i < this.rand(0, 3); i++)
+        for (var i = 0; i < this.rand(1, 3); i++)
         {
             this.rabbitsLeft.push(new Rabbit(this.rand(0, 4), this.rand(0, 10), this.rand(0, 10), this.rand(1, 5), this.rand(1, 5), this.rand(100, 150)));
         }
 
-        for (var i = 0; i < this.rand(0, 3); i++)
+        for (var i = 0; i < this.rand(1, 3); i++)
         {
-            this.rabbitsRight.push(new Rabbit(this.rand(0, 4), this.rand(canvas.width - 10, canvas.width), this.rand(canvas.height - 10, canvas.height), this.rand(-1, -5), this.rand(-1, -5), this.rand(100, 150)));
+            this.rabbitsRight.push(new Rabbit(this.rand(0, 4), this.rand(this.canvas.width - 10, this.canvas.width), this.rand(this.canvas.height - 10, this.canvas.height), this.rand(-1, -5), this.rand(-1, -5), this.rand(100, 150)));
         }
     }
 
-    drawRabbits(ctx, canvas)
+    drawRabbits()
     {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
       
         this.rabbitsLeft.forEach(rabbit => {
-          rabbit.draw(ctx, canvas);
+          rabbit.draw(this.ctx, this.canvas);
         });
 
         this.rabbitsRight.forEach(rabbit => {
-          rabbit.draw(ctx, canvas);
+          rabbit.draw(this.ctx, this.canvas);
         });
     }
 
-    updateRabbits(ctx)
+    updateRabbits()
     {
         this.rabbitsLeft.forEach(rabbit => {
-            rabbit.update(ctx);
+            rabbit.update(this.ctx);
           });
         
           this.rabbitsRight.forEach(rabbit => {
-            rabbit.update(ctx);
+            rabbit.update(this.ctx);
           });
     }
 
-    play(ctx, canvas)
+    drawInterface()
     {
-        this.drawRabbits(ctx, canvas);
-        this.updateRabbits(ctx);
-        requestAnimationFrame(() => this.play(ctx ,canvas));
 
+    }
+
+    play()
+    {
+        //Refresh le canvas
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawRabbits();
+        this.updateRabbits();
     }
 }
