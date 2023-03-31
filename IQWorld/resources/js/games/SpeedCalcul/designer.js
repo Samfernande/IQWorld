4932f006bebbeb52e424cf49944dd3a1cb3244a9
samfernande <samuel.fernandez@eduvaud.ch>
2023-03-31 22:07:13 +0200

    Finition Calculatrice pour SpeedCalcul

--- IQWorld/resources/js/games/SpeedCalcul/designer.js
+++ IQWorld/resources/js/games/SpeedCalcul/designer.js
@@ -19,34 +19,86 @@ export class Designer
     }
 
     createCalculatorButtons() {
-        const calculatorDiv = document.createElement("div");
-        calculatorDiv.classList.add("calculator");
-        for (let i = 9; i > 0; i--) {
+      const calculatorDiv = document.createElement("div");
+      calculatorDiv.classList.add("calculator");
+      for (let i = 9; i > 0; i--) {
           const button = document.createElement("button");
           button.textContent = i;
           button.addEventListener("click", () => {
-            console.log(`You clicked button ${i}`);
+              console.log(`You clicked button ${i}`);
           });
           calculatorDiv.appendChild(button);
-        }
-        const zeroButton = document.createElement("button");
-        zeroButton.classList.add("zero");
-        zeroButton.textContent = 0;
-        zeroButton.addEventListener("click", () => {
-          console.log(`You clicked button 0`);
-        });
-        calculatorDiv.appendChild(zeroButton);
-      
-        const clearButton = document.createElement("button");
-        clearButton.classList.add("clear");
-        clearButton.textContent = "Clear";
-        clearButton.addEventListener("click", () => {
-          console.log(`You clicked the clear button`);
-        });
-        calculatorDiv.appendChild(clearButton);
-      
-        this.gameDiv.appendChild(calculatorDiv);
       }
+  
+      const yoyo = document.createElement("button");
+      yoyo.classList.add("yoyo");
+      yoyo.textContent = "";
+      yoyo.addEventListener("click", () => {
+          console.log(`You clicked the yoyo button`);
+      });
+      calculatorDiv.appendChild(yoyo);
+  
+      const zeroButton = document.createElement("button");
+      zeroButton.classList.add("zero");
+      zeroButton.textContent = 0;
+      zeroButton.addEventListener("click", () => {
+          console.log(`You clicked button 0`);
+      });
+      calculatorDiv.appendChild(zeroButton);
+  
+      const clearButton = document.createElement("button");
+      clearButton.classList.add("clear");
+      clearButton.textContent = "C";
+      clearButton.addEventListener("click", () => {
+          console.log(`You clicked the clear button`);
+      });
+      calculatorDiv.appendChild(clearButton);
+  
+      this.gameDiv.appendChild(calculatorDiv);
+  
+      // Add keydown event listener
+      document.addEventListener('keydown', function(event) {
+          let button = null;
+          switch (event.key) {
+              case '1':
+                  button = document.querySelector('.calculator button:nth-child(9)');
+                  break;
+              case '2':
+                  button = document.querySelector('.calculator button:nth-child(8)');
+                  break;
+              case '3':
+                  button = document.querySelector('.calculator button:nth-child(7)');
+                  break;
+              case '4':
+                  button = document.querySelector('.calculator button:nth-child(6)');
+                  break;
+              case '5':
+                  button = document.querySelector('.calculator button:nth-child(5)');
+                  break;
+              case '6':
+                  button = document.querySelector('.calculator button:nth-child(4)');
+                  break;
+              case '7':
+                  button = document.querySelector('.calculator button:nth-child(3)');
+                  break;
+              case '8':
+                  button = document.querySelector('.calculator button:nth-child(2)');
+                  break;
+              case '9':
+                  button = document.querySelector('.calculator button:nth-child(1)');
+                  break;
+              case '0':
+                  button = document.querySelector('.calculator .zero');
+                  break;
+              case 'Backspace':
+                  button = document.querySelector('.calculator .clear');
+                  break;
+          }
+          if (button) {
+              button.click();
+          }
+      });
+  }
 
     addButton(text, cssClasses = [], cssStyles = {}, position = 'beforeend', clickFunction) {
         const button = document.createElement('button');
